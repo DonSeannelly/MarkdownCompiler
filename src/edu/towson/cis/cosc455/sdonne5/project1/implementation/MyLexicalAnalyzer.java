5b1279aba84984e98dacb03e755f3b6b98f7872d
DonSeannelly <sdonnelly3000@gmail.com>
2015-10-15 11:54:27 -0400

    The bare bones of Project 1: Structure was established with project interfaces and implementation stubs.

---
+++ src/edu/towson/cis/cosc455/sdonne5/project1/implementation/MyLexicalAnalyzer.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package edu.towson.cis.cosc455.sdonne5.project1.implementation;
+
+import edu.towson.cis.cosc455.sdonne5.project1.interfaces.LexicalAnalyzer;
+
+/**
+ * @author sdonne5
+ *
+ */
+public class MyLexicalAnalyzer implements LexicalAnalyzer {
+
+	@Override
+	public void getNextToken() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getCharacter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addCharacter() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isSpace(String c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean lookupToken() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/edu/towson/cis/cosc455/sdonne5/project1/implementation/MySemanticAnalyzer.java
@@ -0,0 +1,5 @@
+package edu.towson.cis.cosc455.sdonne5.project1.implementation;
+
+public class MySemanticAnalyzer {
+
+}
